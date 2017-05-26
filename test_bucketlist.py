6b9016f9819f866e1c7a97b8b1c37def392653b3
Angela Mutava <angiemutava@Angelas-MacBook-Pro.local>
2017-05-26 12:22:57 +0300

    [bucketlist] Added tests for the bucketlist api.

---
+++ test_bucketlist.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+
+
+from app import create_app, db
+
+class BucketListTest(unittest.TestCase):
+    """This class contains tests for bucketlist api and auth"""
+
+	def setUp(self):
+       """This method contains the parameters \
+       that should be setup for each test"""
+		pass
+
+	def tearDown(self):
+		"""This method releases the setup  parameters\
+		 for the consequent tests."""
+		pass	
